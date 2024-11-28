9a65e3fa2ccaf68e59ae76f4e88e94d2b8a21511
dochyun123 <dochyun123@gmail.com>
2024-11-28 14:32:46 +0900

    Add defensive code to prevent animation from freezing

--- vl53l8cx_python/vl53l8cx/visualize.py
+++ vl53l8cx_python/vl53l8cx/visualize.py
@@ -6,6 +6,19 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.colors import ListedColormap
 from collections import deque
+import time
+
+# Check initialzing time
+# driver = VL53L8CX(bus_id=0)
+
+# alive = driver.is_alive()
+# if not alive:
+#     raise IOError("VL53L8CX Device is not alive")
+
+# print("Initialising...")
+# t = time.time()
+# driver.init()
+# print(f"Initialised ({time.time() - t:.1f}s)")
 
 class TOFSensor:
     def __init__(self, resolution=VL53L8CX_RESOLUTION_8X8, frequency=15):
@@ -13,34 +26,42 @@ class TOFSensor:
         self.resolution = resolution
         self.frequency = frequency
         self.res = int(np.sqrt(resolution))
+        self.data_queue = deque(maxlen=64)
+        self.flag = False
         self._initialize_driver()
-        self.data_queue = deque(maxlen=64) 
 
     def _initialize_driver(self):
+        self.flag = True # start initiating
         self.driver.init()
         self.driver.set_resolution(self.resolution)
         self.driver.set_ranging_frequency_hz(self.frequency)
         self.driver.start_ranging()
+        self.flag = False # finish initiating
 
     def get_data(self):
+        if self.flag:
+            return []
         """Collect the latest data from the sensor."""
         if self.driver.check_data_ready():
-            ranging_data = self.driver.get_ranging_data()
-            for i in range(self.resolution):
-                status = ranging_data.target_status[self.driver.nb_target_per_zone * i]
-                distance = ranging_data.distance_mm[self.driver.nb_target_per_zone * i]
+            try:
+                ranging_data = self.driver.get_ranging_data()
+                for i in range(self.resolution):
+                    status = ranging_data.target_status[self.driver.nb_target_per_zone * i]
+                    distance = ranging_data.distance_mm[self.driver.nb_target_per_zone * i]
 
-                data_entry = {
-                    "zone": i,
-                    "Status": status,
-                    "Distance(mm)": int(distance)
-                } 
-                self.data_queue.append(data_entry)
-                with open('data_entries.json', 'a') as f:
-                    json.dump(data_entry, f)
-                    f.write('\n')
-                print(data_entry)
-            return list(self.data_queue)
+                    data_entry = {
+                        "zone": i,
+                        "Status": status,
+                        "Distance(mm)": int(distance)
+                    } 
+                    self.data_queue.append(data_entry)
+                    with open('data_entries.json', 'a') as f:
+                        json.dump(data_entry, f)
+                        f.write('\n')
+                    #print(data_entry)
+                return list(self.data_queue)
+            except Exception as e:
+                self._initialize_driver() # reinitializing
         return []
     
 
@@ -63,7 +84,9 @@ def updatefig(frame, sensor, im, text_objects):
         for i in range(8):
             for j in range(8):
                 text_objects[i][j].set_text(f"{int(data[i, j])}")
-
+    else: # if get no data show the last data
+        print('reinit...')
+    
     return [im] + [text for row in text_objects for text in row]
 
 def main():
