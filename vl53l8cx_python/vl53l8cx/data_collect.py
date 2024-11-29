c3bf9e369c33eacc18e8f054f0e3ba237d283f89
dochyun123 <dochyun123@gmail.com>
2024-11-29 17:45:16 +0900

    Add try-except defense to prevent unexpected stop

--- vl53l8cx_python/vl53l8cx/data_collect.py
+++ vl53l8cx_python/vl53l8cx/data_collect.py
@@ -1,77 +1,79 @@
-import time
 import numpy as np
 from vl53l8cx.vl53l8cx import VL53L8CX
 from .api import *
 import json
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import ListedColormap
+from collections import deque
+import time
+
+# driver = VL53L8CX(bus_id=0)
+
+# alive = driver.is_alive()
+# if not alive:
+#     raise IOError("VL53L8CX Device is not alive")
+
+# print("Initialising...")
+# t = time.time()
+# driver.init()
+# print(f"Initialised ({time.time() - t:.1f}s)")
 
 class TOFSensor:
-    def __init__(self, resolution=VL53L8CX_RESOLUTION_8X8, frequency = 15):
+    def __init__(self, resolution=VL53L8CX_RESOLUTION_8X8, frequency=15):
         self.driver = VL53L8CX(bus_id=0)
         self.resolution = resolution
         self.frequency = frequency
-        self.res = int(np.sqrt(resolution)) 
+        self.res = int(np.sqrt(resolution))
+        self.data_queue = deque(maxlen=64)
+        self.flag = False
         self._initialize_driver()
 
     def _initialize_driver(self):
+        self.flag = True # start initiating
         self.driver.init()
         self.driver.set_resolution(self.resolution)
         self.driver.set_ranging_frequency_hz(self.frequency)
         self.driver.start_ranging()
+        self.flag = False # finish initiating
 
-    def get_coord(self):
-        self.driver.set_resolution(self.resolution)
-        ranging_data = self.driver.get_ranging_data()
-        distance_mm = np.array(ranging_data.distance_mm[:self.resolution]).reshape(self.res, self.res)
-        buf = np.empty((self.res, self.res, 3), dtype=np.float32)
-        per_px = np.deg2rad(45) / self.res
-        it = np.nditer(distance_mm, flags=["multi_index"])
-        for e in it:
-            w, h = it.multi_index # (w,h) = (0,0) ~ (7,7)
-            e = max(e, 0) 
-            x = e * np.cos(w * per_px - np.deg2rad(45) / 2 - np.deg2rad(90)) / 1000  # 2d x 좌표
-            y = e * np.sin(h * per_px - np.deg2rad(45) / 2) / 1000 # 2d y 좌표
-            z = e / 1000 # depth = distance
-            buf[w, h] = [x, y, z]
-        return buf
+    def get_data(self):
+        if self.flag: # if init completes flag is False
+            return []
+        try:
+            if self.driver.check_data_ready():
+                ranging_data = self.driver.get_ranging_data()
+                for i in range(self.resolution):
+                    status = ranging_data.target_status[self.driver.nb_target_per_zone * i]
+                    distance = ranging_data.distance_mm[self.driver.nb_target_per_zone * i]
 
-    def run(self, num_data=10):
-        """Collect data from the ToF sensor and save it to a JSON file."""
-        loop = 0
-        with open("tof_data.json", "a") as json_file:
-            while loop < num_data:
-                if self.driver.check_data_ready():  
-                    ranging_data = self.driver.get_ranging_data()
-                    buf = self.get_coord()  
-                    
-                    for i in range(self.resolution):
-                        status = ranging_data.target_status[self.driver.nb_target_per_zone * i]
-                        distance = ranging_data.distance_mm[self.driver.nb_target_per_zone * i]
-                        w = i // self.res
-                        h = i % self.res
-                        x_coord = round(float(buf[w, h, 0]), 5)
-                        y_coord = round(float(buf[w, h, 1]), 5)
-                        z_coord = round(float(buf[w, h, 2]), 5)
+                    data_entry = {
+                        "zone": i,
+                        "Status": status,
+                        "Distance(mm)": int(distance)
+                    } 
+                    self.data_queue.append(data_entry)
+                    with open('data_entries.json', 'a') as f:
+                        json.dump(data_entry, f)
+                        f.write('\n')
+                    #print(data_entry)
+                return list(self.data_queue)
+        
+        except OSError as e:
+            print(f"I2C error occurred: {e}")
+            self._initialize_driver()  
+            return []
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            self._initialize_driver()  
+            return []
 
-                        # print(f"Zone : {i: >3d}, "
-                        #       f"Status : {status: >3d}, "
-                        #       f"Distance : {distance: >4.0f} mm, "
-                        #       f"X: {x_coord}, "
-                        #       f"Y: {y_coord}, "
-                        #       f"Z: {z_coord}")
 
-                        data_entry = {
-                            "data no": loop,
-                            "zone": i,
-                            "Status": status,  # Status indicating the measurement validity (5 & 9 means ranging OK)
-                            "Distance(mm)": distance                            
-                        }
-
-                        json.dump(data_entry, json_file)
-                        json_file.write('\n')
-                    
-                    loop += 1
-
-if __name__ == '__main__':
+def main():
     sensor = TOFSensor(resolution=VL53L8CX_RESOLUTION_8X8)
-    sensor.run(num_data=10)
-    
+    while True:
+        sensor.get_data()
+
+
+if __name__ == "__main__":
+    main()
